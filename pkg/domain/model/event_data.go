b01cb9030c88166820dcb63706567a9751980df0
Shion Ichikawa <shion1305@gmail.com>
2022-09-19 17:52:00 +0900

    ✏️ refine EventEventData->ExportEventData and csv fields

--- pkg/domain/model/event_data.go
+++ pkg/domain/model/event_data.go
@@ -325,32 +325,33 @@ func accessTest(url string) bool {
 	return true
 }
 
-type EventEventData struct {
-	OriginOrg        string `json:"originOrg"`
-	EventIdMD5       string `json:"id"`
-	EventTitle       string `json:"eventTitle"`
-	EventDescription string `json:"eventDescription"`
-	EventGenreId     int    `json:"eventGenreId"`
-	OrgName          string `json:"orgName"`
-	OrgDescription   string `json:"orgDescription"`
-	SnsTwitter       string `json:"snsTwitter"`
-	SnsFacebook      string `json:"snsFacebook"`
-	SnsInstagram     string `json:"snsInstagram"`
-	SnsWebsite       string `json:"snsWebsite"`
+type ExportEventData struct {
+	EventIdMD5        string `json:"event_id"`
+	EventTitle        string `json:"event_title"`
+	EventSummary      string `json:"event_summary"`
+	EventDescription  string `json:"event_description"`
+	EventDescriptionP string `json:"event_description"`
+	EventGenreId      int    `json:"event_genre_id"`
+	OrgName           string `json:"org_name"`
+	OrgDescription    string `json:"org_description"`
+	SnsTwitter        string `json:"sns_twitter"`
+	SnsFacebook       string `json:"sns_facebook"`
+	SnsInstagram      string `json:"sns_instagram"`
+	SnsWebsite        string `json:"sns_website"`
 }
 
-func (e *EventData) Export() EventEventData {
-	return EventEventData{
-		OriginOrg:        e.originOrg,
-		EventIdMD5:       string(e.eventIdMD5),
-		EventTitle:       e.eventTitle,
-		EventDescription: e.eventDescription,
-		EventGenreId:     e.eventGenre.getEventGenreId(),
-		OrgName:          e.orgName,
-		OrgDescription:   e.orgDescription,
-		SnsTwitter:       e.snsTwitter.getSafeValue(),
-		SnsFacebook:      e.snsFacebook.getSafeValue(),
-		SnsInstagram:     e.snsInstagram.getSafeValue(),
-		SnsWebsite:       e.snsWebsite.getSafeValue(),
+func (e *EventData) Export() ExportEventData {
+	return ExportEventData{
+		EventIdMD5:        string(e.eventIdMD5),
+		EventTitle:        e.eventTitle,
+		EventDescription:  e.eventDescription,
+		EventDescriptionP: e.eventDescriptionP,
+		EventGenreId:      e.eventGenre.getEventGenreId(),
+		OrgName:           e.orgName,
+		OrgDescription:    e.orgDescription,
+		SnsTwitter:        e.snsTwitter.getSafeValue(),
+		SnsFacebook:       e.snsFacebook.getSafeValue(),
+		SnsInstagram:      e.snsInstagram.getSafeValue(),
+		SnsWebsite:        e.snsWebsite.getSafeValue(),
 	}
 }
