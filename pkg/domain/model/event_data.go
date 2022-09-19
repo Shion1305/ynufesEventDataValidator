a2473504b766746c31c0f1226c2063170eabbcb1
Shion Ichikawa <shion1305@gmail.com>
2022-09-19 18:17:48 +0900

    🔥 limit data fields for ExportEventData

--- pkg/domain/model/event_data.go
+++ pkg/domain/model/event_data.go
@@ -345,34 +345,34 @@ func accessTest(url string) bool {
 	return true
 }
 
+// WEB用のExporter
+// 必要のないデータは含まない
 type ExportEventData struct {
-	EventIdMD5        string `json:"event_id"`
-	EventTitle        string `json:"event_title"`
-	EventSummary      string `json:"event_summary"`
-	EventDescription  string `json:"event_description"`
-	EventDescriptionP string `json:"event_description"`
-	EventGenreId      int    `json:"event_genre_id"`
-	OrgName           string `json:"org_name"`
-	OrgDescription    string `json:"org_description"`
-	SnsTwitter        string `json:"sns_twitter"`
-	SnsFacebook       string `json:"sns_facebook"`
-	SnsInstagram      string `json:"sns_instagram"`
-	SnsWebsite        string `json:"sns_website"`
+	EventIdMD5       string `json:"event_id"`
+	EventTitle       string `json:"event_title"`
+	EventSummary     string `json:"event_summary"`
+	EventDescription string `json:"event_description"`
+	EventGenreId     int    `json:"event_genre_id"`
+	OrgName          string `json:"org_name"`
+	OrgDescription   string `json:"org_description"`
+	SnsTwitter       string `json:"sns_twitter"`
+	SnsFacebook      string `json:"sns_facebook"`
+	SnsInstagram     string `json:"sns_instagram"`
+	SnsWebsite       string `json:"sns_website"`
 }
 
 func (e *EventData) Export() ExportEventData {
 	return ExportEventData{
-		EventIdMD5:        string(e.eventIdMD5),
-		EventTitle:        e.eventTitle,
-		EventDescription:  e.eventDescription,
-		EventDescriptionP: e.eventDescriptionP,
-		EventGenreId:      e.eventGenre.getEventGenreId(),
-		OrgName:           e.orgName,
-		OrgDescription:    e.orgDescription,
-		SnsTwitter:        e.snsTwitter.getSafeValue(),
-		SnsFacebook:       e.snsFacebook.getSafeValue(),
-		SnsInstagram:      e.snsInstagram.getSafeValue(),
-		SnsWebsite:        e.snsWebsite.getSafeValue(),
+		EventIdMD5:       string(e.eventIdMD5),
+		EventTitle:       e.eventTitle,
+		EventDescription: e.eventDescription,
+		EventGenreId:     e.eventGenre.getEventGenreId(),
+		OrgName:          e.orgName,
+		OrgDescription:   e.orgDescription,
+		SnsTwitter:       e.snsTwitter.getSafeValue(),
+		SnsFacebook:      e.snsFacebook.getSafeValue(),
+		SnsInstagram:     e.snsInstagram.getSafeValue(),
+		SnsWebsite:       e.snsWebsite.getSafeValue(),
 	}
 }
 
